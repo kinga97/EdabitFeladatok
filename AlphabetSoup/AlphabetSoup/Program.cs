577905f5ce417e974f1e4db9ff919c9709050eb8
kinga97 <kinga.kucsera97@gmail.com>
2020-01-28 14:44:42 +0100

    hatodik mentés

--- AlphabetSoup/AlphabetSoup/Program.cs
+++ AlphabetSoup/AlphabetSoup/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace AlphabetSoup
 {
+    public enum Betuk { a = 1, á, b, c, d, e, é, f, g, h, i, í, j, k, l, m, n, o, ó, ö, ő, p, q, r, s, t, u, ú, ü, ű, v, w, x, y, z, ß }
+
     class Program
     {
         static void Main(string[] args)
@@ -13,8 +15,10 @@ namespace AlphabetSoup
             Console.Write("Kérem a szót: ");
             string bemeno = Console.ReadLine();
 
+            Szoveg szoveg = new Szoveg(bemeno);
             Console.WriteLine($"A szó betűi abc sorrendben: {Sorrendbe_1(bemeno)}");
             Console.WriteLine($"A szó betűi abc sorrendben: {Sorrendbe_2(bemeno)}");
+            Console.WriteLine($"A szó betűi abc sorrendben: {szoveg.Sorrendbe_2()}");
 
             Console.ReadKey(true);
         }
@@ -29,19 +33,22 @@ namespace AlphabetSoup
 
         static string Sorrendbe_2(string bemeno)
         {
-            string kimeno = "{";
+            string kimeno = "ß";
 
             for (int i = 0; i < bemeno.Length; i++)
             {
+                Betuk be;
+                Enum.TryParse(bemeno[i].ToString(), true, out be);
                 for (int j = 0; j < kimeno.Length; j++)
                 {
-                    if (bemeno[i] < kimeno[j])
+                    Betuk ki;
+                    Enum.TryParse(kimeno[j].ToString(), true, out ki);
+                    if ((int)be < (int)ki)
                     {
                         kimeno = String.Concat(kimeno.Substring(0,j), bemeno[i], kimeno.Substring(j));
                         break;
                     }
 
-                        kimeno = String.Concat(kimeno, bemeno[i]);
                 }
             }
 
---
+++ AlphabetSoup/AlphabetSoup/Szoveg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSoup
+{
+    class Szoveg
+    {
+        public string Bemeno { get; set; }
+
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+
+        public string Sorrendbe_2()
+        {
+            string kimeno = "ß";
+
+            for (int i = 0; i < this.Bemeno.Length; i++)
+            {
+                Betuk be;
+                Enum.TryParse(this.Bemeno[i].ToString(), true, out be);
+                for (int j = 0; j < kimeno.Length; j++)
+                {
+                    Betuk ki;
+                    Enum.TryParse(kimeno[j].ToString(), true, out ki);
+                    if ((int)be < (int)ki)
+                    {
+                        kimeno = String.Concat(kimeno.Substring(0, j), this.Bemeno[i], kimeno.Substring(j));
+                        break;
+                    }
+
+                }
+            }
+
+            return kimeno.Substring(0, this.Bemeno.Length);
+        }
+
+    }
+}
